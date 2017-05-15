186c63d422105af89d4632558db07263d67000d4
DomieBett <dbett49@gmail.com>
2017-05-15 21:27:17 +0300

    Added variables, functions, Fellow, Staff and Person classes

---
+++ people.py
@@ -0,0 +1,53 @@
+
+class Person(object):
+
+    def __init__(self, name, role, living_space=False):
+        self.name = name
+        self.role = role
+        self.living_space = living_space
+
+    #Assigns room to specific person depending on whether they
+    #are fellow or staff
+
+    def assign_room():
+        pass
+
+
+class Fellow(Person):
+
+    def __init__(self, name, living_space=False):
+        self.name = name
+        self.role = "Fellow"
+        self.living_space = living_space
+        self.age = 0
+        self.gender = ""
+    
+    # Sets the gender variable
+
+    def set_gender(self, gender):
+        self.gender = gender
+    
+    # Sets the age variable
+
+    def set_age(self, age):
+        self.age = age
+    
+    # Retrieves the gender variable
+
+    def get_gender():
+        if(self.gender==""):
+            return "Gender not assigned"
+        return self.gender
+
+    # Retrieves the age variable
+
+    def get_age():
+        if(self.gender == 0):
+            return "Age not assigned"
+        return self.age
+
+class Staff(Person):
+
+    def __init__(self, name):
+        self.name = name
+        self.role = "Staff"
