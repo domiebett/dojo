0bf81a5c4003a0af94352cccc8986999bb99aa5a
DomieBett <dbett49@gmail.com>
2017-05-17 11:42:17 +0300

    Move functions from subclasses to Room class

--- people.py
+++ people.py
@@ -2,23 +2,28 @@
 class Person(object):
 
     def __init__(self, name, role, living_space=False):
+
         self.name = name
         self.role = role
+        self.id = ""
         self.living_space = living_space
 
     # Sets the gender variable
 
     def set_gender(self, gender):
+
         self.gender = gender
     
     # Sets the age variable
 
     def set_age(self, age):
+
         self.age = age
     
     # Retrieves the gender variable
 
     def get_gender():
+
         if(self.gender==""):
             return "Gender not assigned"
         return self.gender
@@ -26,6 +31,7 @@ class Person(object):
     # Retrieves the age variable
 
     def get_age():
+
         if(self.gender == 0):
             return "Age not assigned"
         return self.age
@@ -34,6 +40,7 @@ class Person(object):
 class Fellow(Person):
 
     def __init__(self, name, living_space=False):
+
         self.name = name
         self.role = "Fellow"
         self.living_space = living_space
@@ -44,6 +51,7 @@ class Fellow(Person):
 class Staff(Person):
 
     def __init__(self, name):
+        
         self.name = name
         self.role = "Staff"
         self.age = 0
