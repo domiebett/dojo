252ea05604a432ab9cc81d56ef87c3d31f5d59d2
DomieBett <dbett49@gmail.com>
2017-05-18 21:03:45 +0300

    Change error message to be more user friendly

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 """
 Usage:
-main.py>> create_room <roomtype> <name>...                                                           
-main.py>> add_person <first_name> <last_name> <person_role> [--a=<want_accomodation>]   
-main.py>> find_userid <first_name> <last_name>                                                       
-main.py>> reallocate_person <person_ID> <room_name>                                               
+main.py>> create_room <roomtype> <name>...
+main.py>> add_person <first_name> <last_name> <person_role> [--a=<want_accomodation>]
+main.py>> find_userid <first_name> <last_name>
+main.py>> reallocate_person <person_ID> <room_name>
 main.py>> load_people <filename>
 main.py>> print_allocations [--o=file_name]
 main.py>> print_unallocated [--o=file_name]
@@ -18,15 +18,14 @@ Options:
     -h, --help  Show this screen and exit.
 """
 
+from docopt import docopt, DocoptExit
+from dojo import Dojo
+from termcolor import cprint
+from pyfiglet import figlet_format
 import sys
 import cmd
 from colorama import init
 init(strip=not sys.stdout.isatty())
-from docopt import docopt, DocoptExit
-from dojo import Dojo
-# from db_conn import DbManager
-from termcolor import cprint
-from pyfiglet import figlet_format
 
 
 def docopt_cmd(func):
@@ -45,7 +44,7 @@ def docopt_cmd(func):
             # The DocoptExit is thrown when the args do not match.
             # We print a message to the user and the usage block.
 
-            print('Invalid Command!')
+            print('You entered the wrong command. Please review Usage')
             print(e)
             return
 
@@ -64,7 +63,6 @@ def docopt_cmd(func):
 
 
 class FrontDojo(cmd.Cmd):
-
 
     header = "  D O J O  "
     cprint(figlet_format(header, font="starwars"), "green")
@@ -87,7 +85,7 @@ class FrontDojo(cmd.Cmd):
 
         """Usage: add_person <first_name> <last_name> <person_role> [--a=<want_accomodation>]"""
         person_name = arg['<first_name>'] + ' ' + arg['<last_name>']
-        if arg['--a'] == None:
+        if arg['--a'] is None:
             want_accomodation = 'N'
         else:
             want_accomodation = str(arg['--a'])
@@ -98,30 +96,25 @@ class FrontDojo(cmd.Cmd):
     def do_find_userid(self, arg):
 
         """Usage: find_userid <first_name> <last_name>"""
-        # person_name = arg['<first_name>'] + ' ' + arg['<last_name>']
-        # print(self.dojo.find_userid(person_name))
         pass
 
     @docopt_cmd
     def do_reallocate_person(self, arg):
 
         """Usage: reallocate_person <person_ID> <room_name>"""
-        # print(self.dojo.reallocate_person(
-        #     int(arg['<person_ID>']), arg['<room_name>']))
         pass
 
     @docopt_cmd
     def do_load_people(self, arg):
 
         """Usage: load_people <filename>"""
-        # self.dojo.load_people(arg['<filename>'])
         pass
 
     @docopt_cmd
     def do_print_allocations(self, args):
 
         '''Usage: print_allocations [--o=filename]'''
-        if args['--o'] == None:
+        if args['--o'] is None:
             output = None
         else:
             output = str(args['--o'])
@@ -132,7 +125,7 @@ class FrontDojo(cmd.Cmd):
     def do_print_unallocated(self, args):
 
         """Usage: print_unallocated [--o=filename]"""
-        if args['--o'] == None:
+        if args['--o'] is None:
             output = None
         else:
             output = str(args['--o'])
@@ -148,15 +141,12 @@ class FrontDojo(cmd.Cmd):
     def do_save_state(self, args):
 
         """Usage: save_state [--db=<sqlite_database>]"""
-        # print(args)
-        # self.dojo.save_state(args)
         pass
 
     @docopt_cmd
     def do_load_state(self, args):
 
         """Usage: load_state [--db=<sqlite_database>]"""
-        # self.dojo.load_state(args)
         pass
 
     def do_quit(self, arg):
