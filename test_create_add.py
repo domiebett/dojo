a53a09324e343f8bd53ee98a90a190772edd003a
DomieBett <dbett49@gmail.com>
2017-05-18 12:10:00 +0300

    Add comments

--- test_create_add.py
+++ test_create_add.py
@@ -22,10 +22,14 @@ class CreateRoomTestCase(unittest.TestCase):
 
         self.office_room.room_occupants[:] = []
 
+    #test if correct name and type are set in rooms
+
     def test_if_correct_room_type_and_name_is_saved(self):
 
         description = [self.office_room.name, self.office_room.room_type]
         self.assertListEqual(description, ["Blue", "office"])
+
+    #test if add_occupant adds occupant to room
 
     def test_if_adding_occupants_increases(self):
 
@@ -35,6 +39,8 @@ class CreateRoomTestCase(unittest.TestCase):
         occupants = len(self.office_room.room_occupants)
         self.assertEqual(occupants, 4)
 
+    #test maximum capacity in rooms is not exceeded
+
     def test_doesnt_add_past_maximum_capacity(self):
 
         for i in range(7):
@@ -42,6 +48,9 @@ class CreateRoomTestCase(unittest.TestCase):
 
         add_extra = self.office_room.add_occupant(self.commuter)
         self.assertEqual(add_extra, "This room is full, try another")
+
+    #test if offices are added into office array and living spaces
+    #into living_space array
 
     def test_rooms_are_added_into_correct_array_in_dojo(self):
 
@@ -51,11 +60,15 @@ class CreateRoomTestCase(unittest.TestCase):
         self.dojo_object.add_room(self.living_space_room)
         self.assertTrue(isinstance(self.office_array[0], Office))
         self.assertTrue(isinstance(living_array[0], LivingSpace))
+    
+    #test if office is an instance of a living space
 
     def test_office_and_livingspace_is_an_instance_of_room(self):
 
         self.assertIsInstance(self.office_room, Room)
         self.assertIsInstance(self.living_space_room, Room)
+    
+    #test if exisiting rooms with same names are not created.
 
     def test_existing_room_is_not_created(self):
         
@@ -73,10 +86,14 @@ class AddPersonTestCase(unittest.TestCase):
         self.staff_member = Staff("Harry")
         self.dojo_object = Dojo()
 
+    #test no staff is given a living space.
+
     def test_staff_are_not_given_living_space(self):
 
         illegal_staff = self.living_space_room.add_occupant(self.staff_member)
         self.assertEqual(illegal_staff, "Staff not allowed in Living Space")
+    
+    #test people without rooms are added to unallocated array
     
     def test_person_is_added_to_unallocated(self):
 
