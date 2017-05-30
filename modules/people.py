d290a136ab3ffba308fde070270308818e17438c
DomieBett <dbett49@gmail.com>
2017-05-30 23:40:25 +0300

    Add id_key property for people

--- modules/people.py
+++ modules/people.py
@@ -1,37 +1,32 @@
+import base64
+
 
 class Person(object):
     """Creates a Person who can either be a Fellow or Staff"""
 
+
     def __init__(self, name, role, living_space=False):
 
+        self.id_key = id(self)
         self.name = name
         self.role = role
-        self.id_key = 0
         self.living_space = living_space
         self.gender = ""
         self.age = 0
-
-    # Sets the gender variable
 
     def set_gender(self, gender):
         """Sets the gender of the person"""
         self.gender = gender
 
-    # Sets the age variable
-
     def set_age(self, age):
         """Sets the age of the person"""
         self.age = age
-
-    # Retrieves the gender variable
 
     def get_gender(self):
         """Returns the gender of the person"""
         if self.gender == "":
             return "Gender not assigned"
         return self.gender
-
-    # Retrieves the age variable
 
     def get_age(self):
         """Returns the age of the person"""
@@ -45,6 +40,7 @@ class Fellow(Person):
 
     def __init__(self, name, living_space=False):
 
+        super().__init__(name, role="Fellow")
         self.name = name
         self.role = "Fellow"
         self.living_space = living_space
@@ -57,6 +53,7 @@ class Staff(Person):
 
     def __init__(self, name):
 
+        super().__init__(name, role="Staff")
         self.name = name
         self.role = "Staff"
         self.age = 0
