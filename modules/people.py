b65b317976149b6316362d250bf74410179caf46
DomieBett <dbett49@gmail.com>
2017-06-10 23:30:49 +0300

    Edit file to pass tests

--- modules/people.py
+++ modules/people.py
@@ -4,33 +4,40 @@ import base64
 class Person(object):
     """Creates a Person who can either be a Fellow or Staff"""
 
-
     def __init__(self, name, role, living_space=False):
 
         self.id_key = id(self)
         self.name = name
         self.role = role
         self.living_space = living_space
-        self.gender = ""
-        self.age = 0
+        self.gender = None
+        self.age = None
 
     def set_gender(self, gender):
         """Sets the gender of the person"""
+
+        if not isinstance(gender, str):
+            return "Should be a string"
         self.gender = gender
 
     def set_age(self, age):
         """Sets the age of the person"""
+
+        if not isinstance(age, int):
+            return "Should be a number"
         self.age = age
 
     def get_gender(self):
         """Returns the gender of the person"""
-        if self.gender == "":
+
+        if self.gender is None:
             return "Gender not assigned"
         return self.gender
 
     def get_age(self):
         """Returns the age of the person"""
-        if self.gender == 0:
+
+        if self.age is None:
             return "Age not assigned"
         return self.age
 
@@ -44,8 +51,6 @@ class Fellow(Person):
         self.name = name
         self.role = "Fellow"
         self.living_space = living_space
-        self.age = 0
-        self.gender = ""
 
 
 class Staff(Person):
@@ -56,5 +61,3 @@ class Staff(Person):
         super().__init__(name, role="Staff")
         self.name = name
         self.role = "Staff"
-        self.age = 0
-        self.gender = ""
