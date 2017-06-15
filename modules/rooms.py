90403b5960450c5517ca6fe0135ea9138e3ea9c7
DomieBett <dbett49@gmail.com>
2017-06-15 11:53:27 +0300

    Remove unneccesary __init__() method

--- modules/rooms.py
+++ modules/rooms.py
@@ -2,19 +2,11 @@ from modules.people import Staff
 
 
 class Room(object):
-    
+
     """Is a room object which contains functions to be imported by
     livingspace and office objects"""
 
-    def __init__(self, room_type, name):
-
-        self.room_type = room_type
-        self.name = name
-        self.maximum_people = 0
-        self.room_occupants = []
-
     def has_space(self):
-        
         """Checks if room has space or if it is full"""
 
         if len(self.room_occupants) < self.maximum_people:
@@ -22,16 +14,15 @@ class Room(object):
         return False
 
     def add_occupant(self, person):
-
         """Adds occupants to the rooms. Also checks if occupant is Staff 
         and denies them access to the Living Space"""
 
         if not self.has_space():
-            print("This room is full, try another")
+            print("   This room is full, try another")
             return "This room is full, try another"
         elif isinstance(self, LivingSpace):
             if isinstance(person, Staff):
-                print("Staff are not allowed in Living Space")
+                print("   Staff are not allowed in Living Space")
                 return "Staff not allowed in Living Space"
             else:
                 self.room_occupants.append(person)
@@ -44,7 +35,7 @@ class LivingSpace(Room):
     """Creates a Living Space object for fellows in the Dojo Facilities"""
 
     def __init__(self, name):
-
+        
         self.name = name
         self.room_type = "living_space"
         self.maximum_people = 4
